5cb32a1b94eb88057306cad055cdad909d316bb2
[josiah stelzl] <josiahstelzl@gmail.com>
2022-11-28 18:59:50 -0500

    worked on bug

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll(
-    //{
+    // {
     //   include: [
     //     Product
     //   ],
